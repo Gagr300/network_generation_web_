cef7c1a4ed288c2f73c7194907085c871542fbda
Gagr300 <gromovag26784@yandex.ru>
2026-05-06 14:48:56 +0400

    evaluate_quality in utils

--- backend/network_generation/utils.py
+++ backend/network_generation/utils.py
@@ -1,5 +1,70 @@
 import networkx as nx
 import numpy as np
+from scipy import stats
+from collections import Counter
+from scipy.spatial import distance
+
+
+def evaluate_quality(g, g_rnd, m, m_rnd):
+    indeg = Counter(d for n, d in g.in_degree())
+    indeg_g_rnd = Counter(d for n, d in g_rnd.in_degree())
+    min_value_indeg = min(min(indeg), min(indeg_g_rnd))
+    max_value_indeg = max(max(indeg), max(indeg_g_rnd))
+    sm_indeg = sum(indeg.values())
+    sm_rnd_indeg = sum(indeg_g_rnd.values())
+
+    outdeg = Counter(d for n, d in g.out_degree())
+    outdeg_g_rnd = Counter(d for n, d in g_rnd.out_degree())
+    min_value_outdeg = min(min(outdeg), min(outdeg_g_rnd))
+    max_value_outdeg = max(max(outdeg), max(outdeg_g_rnd))
+    sm_outdeg = sum(outdeg.values())
+    sm_rnd_outdeg = sum(outdeg_g_rnd.values())
+
+    m_sum = sum(m)
+    m_sum_rnd = sum(m)
+
+    comparison = dict()
+
+    descr = 'Коэффициент корреляции Спирмена для распределения мотивов:'
+    res = stats.spearmanr(m, m_rnd)
+    print(descr, res)
+    comparison[descr] = res
+
+    descr = 'Расстояние Дженсена-Шеннона для распределения мотивов:'
+    res = distance.jensenshannon(
+        [x / m_sum for x in m],
+        [x / m_sum_rnd for x in m_rnd])
+    print(descr, res)
+    comparison[descr] = res
+
+    descr = 'Коэффициент корреляции Спирмена для распределения степеней захода:'
+    res = stats.spearmanr([indeg_g_rnd.get(i, 0) for i in range(min_value_indeg, max_value_indeg + 1)],
+                          [indeg.get(i, 0) for i in range(min_value_indeg, max_value_indeg + 1)])
+    print(descr, res)
+    comparison[descr] = res
+
+    descr = 'Коэффициент корреляции Спирмена для распределения степеней исхода:'
+    res = stats.spearmanr([outdeg_g_rnd.get(i, 0) for i in range(min_value_outdeg, max_value_outdeg + 1)],
+                          [outdeg.get(i, 0) for i in range(min_value_outdeg, max_value_outdeg + 1)])
+    print(descr, res)
+    comparison[descr] = res
+
+    descr = 'Расстояние Дженсена-Шеннона для распределения степеней захода:'
+    res = distance.jensenshannon(
+        [indeg_g_rnd.get(i, 0) / sm_rnd_indeg for i in range(min_value_indeg, max_value_indeg + 1)],
+        [indeg.get(i, 0) / sm_indeg for i in range(min_value_indeg, max_value_indeg + 1)])
+    print(descr, res)
+    comparison[descr] = res
+
+
+    descr = 'Расстояние Дженсена-Шеннона для распределения степеней исхода:'
+    res = distance.jensenshannon(
+        [outdeg_g_rnd.get(i, 0) / sm_rnd_outdeg for i in range(min_value_outdeg, max_value_outdeg + 1)],
+        [outdeg.get(i, 0) / sm_outdeg for i in range(min_value_outdeg, max_value_outdeg + 1)])
+    print(descr, res)
+    comparison[descr] = res
+
+    return comparison
 
 
 def graph_to_json(G):
